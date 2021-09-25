44b1a1ae4ef3f75c19efc1bec1ac1e6ba479a4bb
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-25 14:57:12 -0400

    lesson0 svelte component

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let count = 1
+
+  function increment(){
+    count++
+  }
+</script>
+
+<main class="flex flex-col justify-center items-center h-screen space-y-4">
+  {count}
+  <button on:click="{increment}" class="btn btn-primary">primary</button> 
+</main>
+
+<style>
+</style>
