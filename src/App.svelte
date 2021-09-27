b48fbe77216a98df0dce2c38b9994b6c11b690e3
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-27 16:09:34 -0400

    Lesson 3 setup

--- src/App.svelte
+++ src/App.svelte
@@ -3,21 +3,22 @@
 
   function changeActive(activeNum){
     active = activeNum
+    console.log(activeNum)
   }
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-4">
   <div class="btn-group">
     <button
-      on:click="{changeActive(1)}"
+      on:click={() => changeActive(1)}
       class="btn btn-lg {active === 1 ? 'btn-active' : ''}">Large</button
     >
     <button
-      on:click={changeActive(2)}
+      on:click={() => changeActive(2)}
       class="btn btn-lg {active === 2 ? 'btn-active' : ''}">Large</button
     >
     <button
-      on:click={changeActive(3)}
+      on:click={() => changeActive(3)}
       class="btn btn-lg {active === 3 ? 'btn-active' : ''}">Large</button
     >
   </div>
