095fa6e9025d69e93fd0f5d3fcbfa58ba5479ed7
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-09-28 13:13:51 -0400

    lesson 5 data binding creation

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,52 @@
 <script>
   import data from './data.json'
 
+  let author = '';
+  let post = '';
+
   let posts = data.data
+
+  function addPost(){
+    const _post = {
+      id: posts.length +1,
+      author: author,
+      post: post
+    }
+
+    posts.push(_post)
+    posts = posts
+
+    author = ""
+    post = ""
+  }
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-4">
-    {#each posts as post}
+
+  <h1>Add post</h1>
+  <div class="form-control">
+    <label for="author" class="label">
+      <span class="label-text">Author</span>
+    </label> 
+    <input bind:value="{author}" name="author" type="text" placeholder="username" class="input input-primary input-bordered">
+    <label for="post" class="label">
+      <span class="label-text">Post</span>
+    </label> 
+    <textarea bind:value="{post}" name="post" class="textarea h-24 textarea-bordered textarea-primary" placeholder="Bio"></textarea>
+    <button on:click="{addPost}" class="btn btn-primary mt-4">Submit</button> 
+  </div> 
+
+  <div class="overflow-auto space-y-4 p-4">
+    {#each posts.reverse() as post}
     <div class="card bordered w-80 bg-primary">
       <div class="card-body">
-        <h2 class="card-title">{post.author}</h2> 
-        <p>{post.post}</p> 
-  
+        <h2 class="card-title">{post.author}</h2>
+        <p>{post.post}</p>
       </div>
     </div>
-  {/each}
+    {/each}
+  </div>
+
 </main>
 
 <style>
