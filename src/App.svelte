12d035792f34615d407770267e16b00f671092c2
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-10-05 12:20:25 -0400

    lesson 9 state management added

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,22 @@
 <script>
-  import data from './data.json'
   import Post from './components/Post.svelte'
   import Form from './components/Form.svelte'
   import Nav from './components/Nav.svelte'
-  import {Router, Route, navigate} from 'svelte-navigator'
+  import {Router, Route} from 'svelte-navigator'
+  import { posts } from './stores'
 
-  let posts = data.data
-
-  let arrayInc = 3
-
-  function addPost(author, post){
-    const _post = {
-      id: ++arrayInc,
-      author: author,
-      post: post
-    }
-
-    posts.push(_post)
-    posts = posts
-    navigate('/posts')
-  }
-
-  function deletePost(id){
-    const deleteIndex = posts.findIndex(post => post.id === id)
-    posts.splice(deleteIndex, 1)
-    posts = posts
-  }
 </script>
 
 <Router>
   <main class="flex flex-col justify-center items-center h-screen space-y-4">
     <Nav />
     <Route>
-      <Form addPost={addPost} />
+      <Form />
     </Route>
     <Route path="posts">
       <div class="overflow-auto space-y-4 p-4">
-        {#each posts.reverse() as post}
-        <Post deletePost={deletePost} {...post} />
+        {#each $posts.reverse() as post}
+        <Post {...post} />
         {/each}
       </div>
     </Route>
