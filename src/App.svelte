50002fb13d8c36eaf73f9d53c07732ac52b4efdb
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-10-12 10:17:02 -0400

    added lesson 2 assignment

--- src/App.svelte
+++ src/App.svelte
@@ -12,13 +12,22 @@
   <p>The number is: {number}</p>
   <button on:click="{add5}" class="btn btn-primary">Add 5</button>
   <p>The number mutiplied by 2 is {multiple}</p>
-  {#if number < 30} 
+  {#if number <= 10} 
   <div class="alert alert-success">
     <div class="flex-1">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current">          
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>                
       </svg> 
       <label>The numbers looks good.</label>
+    </div>
+  </div>
+  {:else if number > 10 && number < 30}
+  <div class="alert alert-warning">
+    <div class="flex-1">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 mx-2 stroke-current"> 
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"></path>                         
+      </svg> 
+      <label>The numbers are getting higher!</label>
     </div>
   </div>
   {:else}
