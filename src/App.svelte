3535ccb35ecd934436245bec5073385a45621a24
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-10-12 10:38:11 -0400

    added lesson 4 assignment

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,16 @@
 <script>
-  import data from './data.json'
+  import data from "./data.json";
 
-  let posts = data.data
+  let posts = data.data;
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-4">
-    {#each posts as post}
+  {#each posts as post}
     <div class="card bordered w-80 bg-primary">
       <div class="card-body">
-        <h2 class="card-title">{post.author}</h2> 
-        <p>{post.post}</p> 
-  
+        <h2 class="card-title">{post.author}</h2>
+        <p>{post.date}</p>
+        <p>{post.post}</p>
       </div>
     </div>
   {/each}
