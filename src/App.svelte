60d6feab927362a7e63d6e9e327a744b9bd87296
Mikhail Karan <mikhail.karan@digitaldynasty.ca>
2021-10-12 09:51:56 -0400

    added reactivity assignment solution

--- src/App.svelte
+++ src/App.svelte
@@ -6,12 +6,15 @@
   }
 
   $: multiple = number*2
+
+  $: divide = number/2
 </script>
 
 <main class="flex flex-col justify-center items-center h-screen space-y-4">
   <p>The number is: {number}</p>
   <button on:click="{add5}" class="btn btn-primary">Add 5</button>
   <p>The number mutiplied by 2 is {multiple}</p>
+  <p>The number divided by 2 is {divide}</p>
 </main>
 
 <style>
